aaf8eec2486f0a437f78424a520634a7ee761a4f
yta-br <ytanya.br@gmail.com>
2016-05-17 00:18:21 +0300

    Format text

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -23,8 +23,6 @@ unsigned int HashRs(const byte* data)
 
 void gethash(byte* data)
 {
-//  hash<byte*> data_hash;
-
   std::cout << "data hash is: " << HashRs(data) << '\n';
 }
 // arg1-name interface(init/put_block/get_block)
@@ -33,45 +31,19 @@ int main(int argc, char *argv[])
   int stat = 0;
   uint64 block_size = 15;
   uint32 blob_size = 15;
-  //cout <<"input init param block_size: ";
-  //cin >> block_size;
-  //cout <<"input init param blob_size: ";
-  //cin >> blob_size;
   stat = init(block_size, blob_size);
   while(true)
   {
-    //char cmd_name[10];
-    //cout << "input block command (put/get/exit): ";
-    //cin >>cmd_name;
     string param_str;
-    //string strblock_data;
     byte *block_data = new byte[(unsigned int)block_size];
     uint64 block_id  =1;
-    //if(!strcmp(cmd_name, "put"))
-    {
-      //cout << "input block_id ";
-      //cin >> param_str;
-      //stringstream idStream(param_str);
-      cout << "input block_data ";
-      cin >> param_str;
-      stringstream dataStream(param_str);
-      if(/*(idStream >> block_id) && */(dataStream>>block_data))
-        stat = put_block(block_id, block_data);
-      gethash(block_data);
-    }
-    //else if(!strcmp(cmd_name, "get"))
-    //{
-    //  cout << "input block_id ";
-    //  cin >> param_str;
-    //  stringstream idStream(param_str);
-    //  if(idStream >> block_id)
-    //    stat = get_block(block_id, block_data);
-    //  cout<<block_data<<endl;
-    //}
-    // else if(!strcmp(cmd_name, "exit"))
-    //   break;
+    cout << "input block_data ";
+    cin >> param_str;
+    stringstream dataStream(param_str);
+    if(/*(idStream >> block_id) && */(dataStream>>block_data))
+      stat = put_block(block_id, block_data);
+    gethash(block_data);
     delete[] block_data;
   }
-
   return stat;
 }
